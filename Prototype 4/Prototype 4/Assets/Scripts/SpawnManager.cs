a38c68ec9ddfc2d2a765da6ce34ffdd9c2d8e998
claraep22 <ce00966@srcs.org>
2020-03-14 14:09:55 -0700

    Tutorial 4.4.3 Complete

--- Prototype 4/Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -28,9 +28,9 @@ public class SpawnManager : MonoBehaviour
         return randomPos;
     }
 
-    void SpawnEnemyWave()
+    void SpawnEnemyWave(int enemiesToSpawn)
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < enemiesToSpawn; i++)
         {
             Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
 
